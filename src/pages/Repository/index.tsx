ef4686e213ae24c74ba592590c452f551ade118f
jefferson <jefferson1104junior@gmail.com>
2020-07-06 02:46:40 -0300

    Finish app

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Header, RepositoryInfo, Issues } from './styled';
 
+// interface para tipagem
 interface RepositoryParams {
   repository: string;
 }
 
+// interface para tipagem do typescipt no estado do repositorio
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+// interface para tipagem do typescipt no estado da issues
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  // Iniciando Estado do repositorio e das Issues
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  // Chamadas de api, duas requisicoes ao mesmo tempo, Duas formas de fazer
+  useEffect(() => {
+    /* PRIMEIRA FORMA UTILIZANDO O THEN */
+    // Requisicao dos dados do repositorio
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+
+    // Requisicao das issues do repositorio
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+
+    /* SEGUNDA FORMA UTILIZANDO FUNCAO ASINCRONA COM PROMISE.ALL
+    async function loadData(): Promise<void> {
+      const [repository, issues] = await Promise.all([
+        api.get(`repos/${params.repository}`),
+        api.get(`repos/${params.repository}/issues`),
+      ]);
+
+      console.log(repository);
+      console.log(issues);
+    }
+
+    loadData();
+    */
+  }, [params.repository]);
 
   return (
     <>
@@ -23,42 +80,46 @@ const Repository: React.FC = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
-            alt="Jefferson Soares"
-          />
-          <div>
-            <strong>Jefferson1104/repository</strong>
-            <p>Um Repositorio de Jefferson Soares Costa Junior</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1800</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>50</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
 
       <Issues>
-        <Link to="asdc">
-          <div>
-            <strong>asdacascas</strong>
-            <p>asdaaec</p>
-          </div>
+        {issues.map(issue => (
+          <a href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
