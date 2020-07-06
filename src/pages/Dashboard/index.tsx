fda3b7e7b4c8ac0351929b6bd0315551c1f3258c
jefferson <jefferson1104junior@gmail.com>
2020-07-05 22:37:42 -0300

    dashboad styled

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styled';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styled';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
+            alt="Jefferson Soares"
+          />
+
+          <div>
+            <strong>Jefferson1104/repo</strong>
+            <p>Um repositório de Jefferson Soares Costa Junior</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
+            alt="Jefferson Soares"
+          />
+
+          <div>
+            <strong>Jefferson1104/repo</strong>
+            <p>Um repositório de Jefferson Soares Costa Junior</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
+            alt="Jefferson Soares"
+          />
+
+          <div>
+            <strong>Jefferson1104/repo</strong>
+            <p>Um repositório de Jefferson Soares Costa Junior</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
