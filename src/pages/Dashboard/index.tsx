1d103a807d32c7489d842dc6354caba8e94977d3
jefferson <jefferson1104junior@gmail.com>
2020-07-06 00:34:48 -0300

    working errors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,61 +1,93 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styled';
+import { Title, Form, Repositories, Error } from './styled';
+
+// interface para a tipagem do typescript, tipagem para o Estado repositories e setRepositories
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  // Estado, variavel para gravar o valor digitado no campo input, e variavel para alterar o valor
+  const [newRepo, setNewRepo] = useState('');
+
+  // Estado para controle de erros
+  const [inputError, setInputError] = useState('');
+
+  // Estado, vetores(arrays) onde vamos armazer os repositorios
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  // funcao para adicionar novos repositorios
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    // Tratando erro do campo input vazio
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      // consumindo api do github
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      // salvando novo repositorio no array
+      setRepositories([...repositories, repository]);
+
+      // limpando campo input apos pesquisar
+      setNewRepo('');
+
+      // limpando erro caso fazer uma busca correta
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
-            alt="Jefferson Soares"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <div>
-            <strong>Jefferson1104/repo</strong>
-            <p>Um repositório de Jefferson Soares Costa Junior</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
-            alt="Jefferson Soares"
-          />
-
-          <div>
-            <strong>Jefferson1104/repo</strong>
-            <p>Um repositório de Jefferson Soares Costa Junior</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/48356215?s=460&u=71601327f3c8678b4357d31e56428c06f76c598b&v=4"
-            alt="Jefferson Soares"
-          />
-
-          <div>
-            <strong>Jefferson1104/repo</strong>
-            <p>Um repositório de Jefferson Soares Costa Junior</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -1,5 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+// interface para informar ao typescript que o Form aceita a propriedade hasError
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -11,7 +16,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -22,6 +27,15 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0px 0px 5px;
+    color: #3a4a4a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -43,6 +57,12 @@ export const Form = styled.form`
       background: ${shade(0.3, '#5990e3')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -77,7 +97,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
